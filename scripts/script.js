0f9b85a43781d5072a98c1b6274316b99ae1f701
yudielgc <yudielgc@gmail.com>
2016-04-10 23:35:47 -0400

    skeleton for main layout

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,19 @@
 (function($) {
 
   $.fn.menumaker = function(options) {
-      
-      var cssmenu = $(this), settings = $.extend({
+
+      var menu = $(this), settings = $.extend({
         title: "Menu",
         format: "dropdown",
         sticky: false
       }, options);
 
       return this.each(function() {
-        cssmenu.prepend('<div id="menu-button">' + settings.title + '</div>');
+        menu.prepend('<div id="menu-button">' + settings.title + '</div>');
         $(this).find("#menu-button").on('click', function(){
           $(this).toggleClass('menu-opened');
           var mainmenu = $(this).next('ul');
-          if (mainmenu.hasClass('open')) { 
+          if (mainmenu.hasClass('open')) {
             mainmenu.hide().removeClass('open');
           }
           else {
@@ -24,11 +24,11 @@
           }
         });
 
-        cssmenu.find('li ul').parent().addClass('has-sub');
+        menu.find('li ul').parent().addClass('has-sub');
 
         multiTg = function() {
-          cssmenu.find(".has-sub").prepend('<span class="submenu-button"></span>');
-          cssmenu.find('.submenu-button').on('click', function() {
+          menu.find(".has-sub").prepend('<span class="submenu-button"></span>');
+          menu.find('.submenu-button').on('click', function() {
             $(this).toggleClass('submenu-opened');
             if ($(this).siblings('ul').hasClass('open')) {
               $(this).siblings('ul').removeClass('open').hide();
@@ -40,17 +40,17 @@
         };
 
         if (settings.format === 'multitoggle') multiTg();
-        else cssmenu.addClass('dropdown');
+        else menu.addClass('dropdown');
 
-        if (settings.sticky === true) cssmenu.css('position', 'fixed');
+        if (settings.sticky === true) menu.css('position', 'fixed');
 
         resizeFix = function() {
           if ($( window ).width() > 768) {
-            cssmenu.find('ul').show();
+            menu.find('ul').show();
           }
 
           if ($(window).width() <= 768) {
-            cssmenu.find('ul').hide().removeClass('open');
+            menu.find('ul').hide().removeClass('open');
           }
         };
         resizeFix();
@@ -63,7 +63,7 @@
 (function($){
 $(document).ready(function(){
 
-$("#cssmenu").menumaker({
+$("#menu").menumaker({
    title: "Menu",
    format: "multitoggle"
 });
